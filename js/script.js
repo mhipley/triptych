cfc2661d58d214c57761b5bc997cda528e0725df
Martha Hipley <info@marthahipley.com>
2020-09-15 19:09:24 -0500

    update #1, still missing pool animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("hello world!");
+// $(document).ready(function(){
+//     console.log("hello world!!");
+
+//     var containerHeight = $('#calm .bottom').height(),
+//     containerWidth = $('#calm .bottom').width(),
+//     $div = $('.wave'),
+//     divWidth = $div.width(),
+//     divHeight = $div.height(),
+//     heightMax = containerHeight - divHeight,
+//     widthMax = containerWidth - divWidth;
+
+//     console.log(containerHeight);
+
+//     $div.each(function(i, obj) {
+//         console.log(obj);
+//         $(obj).css({
+//             left: Math.floor( Math.random() * widthMax ),
+//             top: Math.floor( Math.random() * heightMax )
+//         });
+//     });
+
+
+
+// });
